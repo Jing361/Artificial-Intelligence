349d4329f2a7cee359a54e9bf07da3161f3854c7
Chris Samuelson <chris.sam55@gmail.com>
2013-10-20 21:57:56 +0000

    Prepping for project2.

---
+++ project2/src/P2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# P1.py
+# Author: Paul Talaga
+# 
+# This file implements a sudoku solver
+# deterministic, static, discrete world.
+
+############################################
+## A few grid configurations
+
+#############################################    
+def main():
+  print("testing")
+  
+
+if __name__ == "__main__":
+  # This code will be run if this file is called on its own
+  main()
+
