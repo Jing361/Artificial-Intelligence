8a227faf4672fd1c62f125ccd3446bd049790862
Chris Samuelson <chris.sam55@gmail.com>
2013-10-20 18:01:34 -0400

    Sample input files.

--- project1/src/P1.py
+++ project1/src/P1.py
@@ -7,6 +7,10 @@
 
 from roomba_sim import *
 from roomba_concurrent import *
+try:
+  import Queue
+except ImportError:
+  import queue as Queue
 
 import time
 
@@ -20,10 +24,15 @@ class aStarRobot(DiscreteRobot):
   # [(xr, yr), []]
 
   def initialize(self, chromosome):
+    # Setup action, state, and node formats
+    # Action format is a list
     self.actionlist = []
+    # A state is a 2-tuple of position and a list of dirty tiles
     x, y = self.getRobotPosition()
     state = ((int(x), int(y)), self.getDirty())
+    # A node is a 2-tuple of the action list and the state
     node = ([], state)
+    # Search returns a node
     actionList, state = self.Astar(node)
     actionList.reverse()
     self.actionlist = actionList
@@ -34,62 +43,118 @@ class aStarRobot(DiscreteRobot):
   def h(self, node):# Heuristic
     actionList, state = node
     position, dirtList = state
-    numDirt = len(dirtList) + len(actionList)*0.1
+    # The more dirt remaining, the less we want to go this way
+    # The 
+    numDirt = len(dirtList) + len(actionList) * 0.1
     return numDirt
 
-  def generateSuccessors(self, fNode):
+  def generateSuccessors(self, heurNode):
+    # Generate all possible successors to the current position
+    # If it can move down, create a 'down' node to explore
+    # 'children' is the list of successors that will be returned
+    # Clean current space, if dirty
+    # For each cleaned space, update list of dirt
+    # Branch in all available directions
+    # For each branch update direction and add action to list
+    
+    # Rip up heurNode for useful parts
     children = []
-    hVal, node = fNode
+    heurVal, node = heurNode
     actionList, state = node
     position, dirtList = state
     wallList = self.getWalls()
     x, y = position
+    # Generate new positions after movement
     north = (int(x), int(y + 1))
     south = (int(x), int(y - 1))
     east =  (int(x + 1), int(y))
     west =  (int(x - 1), int(y))
+    # Check if cleaning needs to be done
     if position in dirtList:
       newCleaned = dirtList.copy()
       newCleaned.remove(position)
       stateSu = (position, newCleaned)
       nodeSu = (actionList + ['Suck'], stateSu)
       return [nodeSu]
+    # Check if each direction is valid(not a wall)
     if north not in wallList:
       stateN = (north, dirtList)
-      nodeN = (actionList + ['North'], stateN)
+      newList = actionList.copy()
+      newList.append('North')
+      nodeN = (newList, stateN)
       children.append(nodeN)
     if south not in wallList:
       stateS = (south, dirtList)
-      nodeS = (actionList + ['South'], stateS)
+      newList = actionList.copy()
+      newList.append('South')
+      nodeS = (newList, stateS)
       children.append(nodeS)
     if east not in wallList:
       stateE = (east, dirtList)
-      nodeE = (actionList + ['East'], stateE)
+      newList = actionList.copy()
+      newList.append('East')
+      nodeE = (newList, stateE)
       children.append(nodeE)
     if west not in wallList:
       stateW = (west, dirtList)
-      nodeW = (actionList + ['West'], stateW)
+      newList = actionList.copy()
+      newList.append('West')
+      nodeW = (newList, stateW)
       children.append(nodeW)
     return children
 
   def getHash(self, node):
-    aList, state = node
+    # Create a unique identifier for the state in node
+    #  Be sure to ignore actionlist
+    
+    # Rip up the node
+    actions, state = node
     position, dirtList = state
     h = hash(frozenset(dirtList))
     return(str(position) + str(h))
 
   def Astar(self, node):
-    frontier = [(self.h(node), node)]
+    # Generate action list to clean given space
+    # A priority queue using the heuristic
+    #  as the priority index will implement A*
+    frontier = Queue.PriorityQueue()
+    frontier.put((self.h(node), node))
     explored = []
     maxexplored = 0
     expansions = 0
-    while (not len(frontier) == 0):
+    while not frontier.empty():
       expansions += 1
-      thisnode = frontier.pop()
+      thisnode = frontier.get()
       if thisnode == None:
         print("Broke, bad node")
         return None
-      hVal, nNode = thisnode
+      heurVal, nNode = thisnode
+      aList, state = nNode
+      explored.append(self.getHash(nNode))
+      position, dirt = state
+      # Check goal
+      if len(dirt) == 0:
+        return nNode
+      for child in self.generateSuccessors(thisnode):
+        if self.getHash(child) in explored:
+          continue
+        frontier.put((self.h(child), child))
+        if maxexplored < len(explored):
+          maxexplored = len(explored)
+  
+  def dfs(self, node, maxdepth = 999999):
+    frontier = Queue.Queue()
+    frontier.put(node)
+    explored = []
+    maxexplored = 0
+    expansions = 0
+    while not frontier.empty():
+      expansions += 1
+      thisnode = frontier.get()
+      if thisnode == None:
+        print("Broke, bad node")
+        return None
+      heurVal, nNode = thisnode
       aList, state = nNode
       explored.append(self.getHash(nNode))
       position, dirt = state
@@ -98,53 +163,32 @@ class aStarRobot(DiscreteRobot):
       for child in self.generateSuccessors(thisnode):
         if self.getHash(child) in explored:
           continue
-        frontier.insert(1, (self.h(child), child))
-        if maxexplored < len(explored):
-          maxexplored = len(explored)
-  
-  def dfs(self, node, maxdepth = 999999):
-    frontier = [node]
-    explored = {}
-    maxexplored = 0
-    expansions = 0
-    while (1):
-      if len(frontier) == 0:
-        return None
-      thisnode = frontier.pop()
-      explored[self.getHash(thisnode)] = 1
-      expansions += 1
-      for newnode in self.generateSuccessors(thisnode):
-        if len(newnode[1]) > maxdepth:
-          continue;
-        if self.getHash(newnode) in explored:
-          continue;
-        if self.isGoal(newnode):
-          return (newnode, maxexplored, expansions)
-        frontier.append(newnode)
+        frontier.put((self.h(child), child))
         if maxexplored < len(explored):
           maxexplored = len(explored)
 
   def bfs(self, node):
-    frontier = [node]
+    frontier = []
+    frontier.append(node)
     explored = []
     maxexplored = 0
     expansions = 0
-    while (not len(frontier) == 0):
-      if len(frontier) == 0:
-        print("Broken, frontier is zero")
-        return None
+    while not len(frontier) == 0:
+      expansions += 1
       thisnode = frontier.pop()
       if thisnode == None:
-        print("Broken, new node is invalid")
+        print("Broke, bad node")
         return None
-      explored.append(self.getHash(thisnode))
-      expansions += 1
-      for newnode in self.generateSuccessors(thisnode):
-        if self.getHash(newnode) in explored:
-          continue;
-        if self.isGoal(newnode):
-          return (newnode, maxexplored, expansions)
-        frontier.insert(1, newnode)
+      heurVal, nNode = thisnode
+      aList, state = nNode
+      explored.append(self.getHash(nNode))
+      position, dirt = state
+      if len(dirt) == 0:
+        return nNode
+      for child in self.generateSuccessors(thisnode):
+        if self.getHash(child) in explored:
+          continue
+        frontier.append((self.h(child), child))
         if maxexplored < len(explored):
           maxexplored = len(explored)
     
@@ -192,28 +236,18 @@ mediumWalls5Room.setWall((26,25), (7,25))
 mediumWalls5Room.setWall((7,5), (7,22))
 allRooms.append(mediumWalls5Room) # [7]
 
-boopRoom = RectangularRoom(2, 2, .5)
-
 #############################################    
 def aStar():
-  print(runSimulation(num_trials = 2,
-                    room = allRooms[0],
+  print(runSimulation(num_trials = 1,
+                    room = allRooms[7],
                     robot_type = aStarRobot,
                     ui_enable = True,
                     ui_delay = 0.01))
-                    
-def test():
-    print(runSimulation(num_trials = 1,
-                        room = boopRoom,
-                        robot_type = aStarRobot,
-                        ui_enable = True))
 
 if __name__ == "__main__":
   # This code will be run if this file is called on its own
-  test()
-  #aStar()
+  aStar()
   
   # Concurrent test execution.
   #concurrent_test(aStarRobot, [allRooms[0], allRooms[1], allRooms[2]], 10)
   #testAllMaps(aStarRobot, [allRooms[0], allRooms[1], allRooms[2]], 2)
-
